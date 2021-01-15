d54b2fa6caa7bf7ca9244a2462b5c647c3bfff85
Daniel Sheeler <dsheeler@pobox.com>
2021-01-15 14:07:43 -0600

    try using shell script for testing

---
+++ tests/test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+commands=(
+  "python -m unittest discover tests"
+  "mpirun -n 9 python -m unittest tests.shared_obj_tests"
+  "mpirun -n 9 python -m unittest tests.shared_vl_tests"
+  "mpirun -n 9 python -m unittest tests.shared_vl_tests"
+  "mpirun -n 18 python -m unittest tests.shared_obj_tests.SharedGridTests.test_buffer_data_3d"
+  "mpirun -n 18 python -m unittest tests.shared_obj_tests.SharedGridTests.test_buffer_data_3d_periodic"
+  "mpirun -n 18 python -m unittest tests.shared_vl_tests.SharedValueLayerTests.test_buffers_3x3x3_periodic"
+  "mpirun -n 18 python -m unittest tests.shared_vl_tests.SharedValueLayerTests.test_buffers_3x3x3_sticky"
+)
+
+declare -i result
+
+for cmd in "${commands[@]}"
+do
+  echo $cmd
+  echo ""
+  $cmd
+  (( result = $result + $? ))
+  echo ""
+done
+
+exit $result
