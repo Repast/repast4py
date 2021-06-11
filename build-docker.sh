a52fc41d35116472105949e3a5abb86f206210e5
Daniel Sheeler <dsheeler@pobox.com>
2021-06-11 08:10:43 -0500

    p2m4py -> repast4py

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
 docker build --rm -f Dockerfile -t repast4py:latest .
-docker tag p2m4py:latest dsheeler/repast4py:latest
+docker tag repast4py:latest dsheeler/repast4py:latest
 docker push dsheeler/repast4py:latest
