b3620d8601433b8a141011036e17e56aa51a6e0c
Nick Collier <ncollier@anl.gov>
2021-08-11 16:50:00 -0400

    Separated walking and counting

--- examples/rndwalk/rndwalk.py
+++ examples/rndwalk/rndwalk.py
@@ -35,12 +35,14 @@ class Walker(core.Agent):
         """
         return (self.uid, self.meet_count, self.pt.coordinates)
 
-    def walk(self, grid, meet_log: MeetLog):
+    def walk(self, grid):
         # choose two elements from the OFFSET array
         # to select the direction to walk in the
         # x and y dimensions
         xy_dirs = rng.choice(Walker.OFFSETS, size=2)
         self.pt = grid.move(self, dpt(self.pt.x + xy_dirs[0], self.pt.y + xy_dirs[1], 0))
+
+    def count_colocations(self, grid, meet_log: MeetLog):
         # subtract self
         num_here = grid.get_num_agents(self.pt) - 1
         meet_log.total_meets += num_here
@@ -78,8 +80,8 @@ def restore_walker(walker_data: Tuple):
 
 class Model:
     """
-    The Model class encapsulates the simulation, and is 
-    responsible for intialization (scheduling events, creating agents,
+    The Model class encapsulates the simulation, and is
+    responsible for initialization (scheduling events, creating agents,
     and the grid the agents inhabit), and the overall iterating
     behavior of the model.
 
@@ -104,7 +106,7 @@ class Model:
         box = space.BoundingBox(0, params['world.width'], 0, params['world.height'], 0, 0)
         # create a SharedGrid of 'box' size with sticky borders that allows multiple agents
         # in each grid location.
-        self.grid = space.SharedGrid(name='grid', bounds=box, borders=space.BorderType.Sticky, 
+        self.grid = space.SharedGrid(name='grid', bounds=box, borders=space.BorderType.Sticky,
                                      occupancy=space.OccupancyType.Multiple, buffersize=2, comm=comm)
         self.context.add_projection(self.grid)
 
@@ -118,18 +120,29 @@ class Model:
             self.grid.move(walker, pt)
 
         # initialize the logging
+        self.agent_logger = logging.TabularLogger(comm, params['agent_log_file'], ['tick', 'agent_id', 'agent_uid_rank', 'meet_count'])
+
         self.meet_log = MeetLog()
         loggers = logging.create_loggers(self.meet_log, op=MPI.SUM, names={'total_meets': 'total'}, rank=rank)
         loggers += logging.create_loggers(self.meet_log, op=MPI.MIN, names={'min_meets': 'min'}, rank=rank)
         loggers += logging.create_loggers(self.meet_log, op=MPI.MAX, names={'max_meets': 'max'}, rank=rank)
         self.data_set = logging.ReducingDataSet(loggers, MPI.COMM_WORLD, params['meet_log_file'])
 
-        self.agent_logger = logging.TabularLogger(comm, params['agent_log_file'], ['tick', 'agent_id', 'meet_count'])
+        # count the initial colocations at time 0 and log
+        for walker in self.context.agents():
+            walker.count_colocations(self.grid, self.meet_log)
+        self.data_set.log(0)
+        self.meet_log.max_meets = self.meet_log.min_meets = self.meet_log.total_meets = 0
+        self.log_agents()
 
     def step(self):
         for walker in self.context.agents():
-            walker.walk(self.grid, self.meet_log)
+            walker.walk(self.grid)
+
         self.context.synchronize(restore_walker)
+
+        for walker in self.context.agents():
+            walker.count_colocations(self.grid, self.meet_log)
 
         tick = self.runner.schedule.tick
         self.data_set.log(tick)
@@ -139,8 +152,7 @@ class Model:
     def log_agents(self):
         tick = self.runner.schedule.tick
         for walker in self.context.agents():
-            id_str = f'{walker.id}_{walker.uid_rank}'
-            self.agent_logger.log_row(tick, id_str, walker.meet_count)
+            self.agent_logger.log_row(tick, walker.id, walker.uid_rank, walker.meet_count)
 
         self.agent_logger.write()
 
